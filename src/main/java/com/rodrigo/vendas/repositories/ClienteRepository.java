c9e233b60e78a4fc1e63528707761b0faa67c204
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-10-14 10:38:22 -0300

    Trabalhando com @Query

--- src/main/java/com/rodrigo/vendas/repositories/ClienteRepository.java
+++ src/main/java/com/rodrigo/vendas/repositories/ClienteRepository.java
@@ -1,11 +1,21 @@
 package com.rodrigo.vendas.repositories;
 
+import java.util.List;
+
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.rodrigo.vendas.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	
+	@Query(name="SELECT C FROM Cliente c WHERE c.nome LIKE :nome")
+	List<Cliente> encontrarPorNome(@Param(value="nome") String nome);
 
 }
