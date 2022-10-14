2975b5f18a5f242e67d569a0b95dbcfd02b1c507
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-10-14 09:42:47 -0300

    Introdução aos repositórios Spring Data

---
+++ src/main/java/com/rodrigo/vendas/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.rodrigo.vendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rodrigo.vendas.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
