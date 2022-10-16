c501be4b1fad9b4ed190ebd8477d4c5504044e60
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-10-16 15:18:22 -0300

    Carregando os pedidos por cliente

--- src/main/java/com/rodrigo/vendas/repositories/PedidoRepository.java
+++ src/main/java/com/rodrigo/vendas/repositories/PedidoRepository.java
@@ -1,12 +1,17 @@
 package com.rodrigo.vendas.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.rodrigo.vendas.domain.Cliente;
 import com.rodrigo.vendas.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+	
+	List<Pedido> findByCliente(Cliente cliente);
 	
 	
 
